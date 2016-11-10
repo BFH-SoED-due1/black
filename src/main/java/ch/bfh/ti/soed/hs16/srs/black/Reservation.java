a6b1b2a7d4d3a81387f2998f29826fcadf1b934c
Davide Papacella <davide.papacella@students.bfh.ch>
2016-11-10 14:57:10 +0100

    changed date format to Date in Reservation.java and updated unit tests

--- src/main/java/ch/bfh/ti/soed/hs16/srs/black/Reservation.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/black/Reservation.java
@@ -1,19 +1,21 @@
 package ch.bfh.ti.soed.hs16.srs.black;
 
+import java.util.Date;
+
 public class Reservation implements Comparable<Reservation>{
     private final Customer customer;
     private final Room room;
-    private final long startTime;
-    private final long endTime;
+    private final Date begin;
+    private final Date end;
     // price class
 
-    public Reservation(Customer customer, Room room, long startTime, long endTime){
-        customer.addReservation(this);
-        room.addReservation(this);
+    public Reservation(Customer customer, Room room, Date begin, Date end) {
         this.customer = customer;
         this.room = room;
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.begin = begin;
+        this.end = end;
+        customer.addReservation(this);
+        room.addReservation(this);
     }
 
     public void cancelReservation(){
@@ -23,10 +25,10 @@ public class Reservation implements Comparable<Reservation>{
 
     @Override
     public int compareTo(Reservation o) {
-        if (startTime == o.startTime)
-            return 0;
-        if (startTime - o.startTime > 0)
+        if (begin.before(o.begin))
+            return -1;
+        if (begin.after(o.begin))
             return 1;
-        return -1;
+        return 0;
     }
 }
--- src/test/java/ch/bfh/ti/soed/hs16/srs/black/ReservationTest.java
+++ src/test/java/ch/bfh/ti/soed/hs16/srs/black/ReservationTest.java
@@ -1,45 +1,60 @@
 package ch.bfh.ti.soed.hs16.srs.black;
 
+
+import org.junit.Before;
 import org.junit.Test;
-import java.util.Set;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import static org.junit.Assert.*;
 
 public class ReservationTest {
+    // some test dates: date1 < date2 < date3 < date4
+    private Date date1;
+    private Date date2;
+    private Date date3;
+    private Date date4;
+
+    @Before
+    public void setUpTestDates(){
+        Calendar c = new GregorianCalendar();
+        c.set(2016, 11, 9, 13, 22, 15);
+        date1 = new Date(c.getTimeInMillis());
+        c.set(2016, 11, 9, 15, 22, 15);
+        date2 = new Date(c.getTimeInMillis());
+        c.set(2016, 11, 9, 16, 22, 15);
+        date3 = new Date(c.getTimeInMillis());
+        c.set(2016, 11, 9, 17, 22, 15);
+        date4 = new Date(c.getTimeInMillis());
+    }
+
     @Test
     public void testCompareReservations() throws Exception {
         int returnValue, expected;
-        long startTimeReservation1;
-        long startTimeReservation2;
         Room testRoom1 = new Room(1, "50m^2");
         Customer testCustomer1 = new Customer("Albert");
         Reservation reservation1;
         Reservation reservation2;
 
         // Test if Reservation1 and Reservation2 begin at the same time
-        startTimeReservation1 = 100;
-        startTimeReservation2 = startTimeReservation1;
-        reservation1 = new Reservation(testCustomer1, testRoom1, startTimeReservation1, 200);
-        reservation2 = new Reservation(testCustomer1, testRoom1, startTimeReservation2, 200);
+        reservation1 = new Reservation(testCustomer1, testRoom1, date1, date2);
+        reservation2 = new Reservation(testCustomer1, testRoom1, date1, date2);
         returnValue = reservation1.compareTo(reservation2);
-        expected = (startTimeReservation1 == startTimeReservation2)?0:1;
+        expected = 0; // both starts with date1
         assertEquals(returnValue, expected);
 
         // Test if Reservation1 begins later then Reservation2
-        startTimeReservation1 = 100;
-        startTimeReservation2 = 99;
-        reservation1 = new Reservation(testCustomer1, testRoom1, startTimeReservation1, 200);
-        reservation2 = new Reservation(testCustomer1, testRoom1, startTimeReservation2, 200);
+        reservation1 = new Reservation(testCustomer1, testRoom1, date2, date3);
+        reservation2 = new Reservation(testCustomer1, testRoom1, date1, date4);
         returnValue = reservation1.compareTo(reservation2);
-        expected = (startTimeReservation1 > startTimeReservation2)?1:-1;
+        expected = 1;
         assertEquals(returnValue, expected);
 
         // Test if Reservation2 begins later then Reservation1
-        startTimeReservation1 = 100;
-        startTimeReservation2 = 101;
-        reservation1 = new Reservation(testCustomer1, testRoom1, startTimeReservation1, 200);
-        reservation2 = new Reservation(testCustomer1, testRoom1, startTimeReservation2, 200);
+        reservation1 = new Reservation(testCustomer1, testRoom1, date1, date3);
+        reservation2 = new Reservation(testCustomer1, testRoom1, date2, date4);
         returnValue = reservation1.compareTo(reservation2);
-        expected = (startTimeReservation1 < startTimeReservation2)?-1:1;
+        expected = -1;
         assertEquals(returnValue, expected);
     }
 
@@ -47,9 +62,7 @@ public class ReservationTest {
     public void testCreateReservation() throws Exception {
         Room testRoom1 = new Room(1,"50m^2");
         Customer testCustomer1 = new Customer("Albert");
-
-        Reservation testReservation1 = new Reservation(testCustomer1, testRoom1, 100, 200);
-        Set<Reservation> test = testRoom1.getReservations();
+        Reservation testReservation1 = new Reservation(testCustomer1, testRoom1, date1, date2);
         assertTrue(testRoom1.getReservations().contains(testReservation1));
         assertTrue(testCustomer1.getReservations().contains(testReservation1));
     }
@@ -58,11 +71,9 @@ public class ReservationTest {
     public void testCancelReservation() throws Exception {
         Room testRoom1 = new Room(1,"50m^2");
         Customer testCustomer1 = new Customer("Albert");
-        Reservation testReservation1 = new Reservation(testCustomer1, testRoom1, 100, 200);
-        Set<Reservation> test = testRoom1.getReservations();
+        Reservation testReservation1 = new Reservation(testCustomer1, testRoom1, date1, date2);
         assertTrue(testRoom1.getReservations().contains(testReservation1));
         assertTrue(testCustomer1.getReservations().contains(testReservation1));
-
         testReservation1.cancelReservation();
         assertFalse(testRoom1.getReservations().contains(testReservation1));
         assertFalse(testCustomer1.getReservations().contains(testReservation1));
