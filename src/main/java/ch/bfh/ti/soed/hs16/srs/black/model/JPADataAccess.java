631b27889e95d82888541067f32a3bf026148714
Davide Papacella <davide.papacella@students.bfh.ch>
2016-12-01 14:08:49 +0100

    fixed checkstyle

--- src/main/java/ch/bfh/ti/soed/hs16/srs/black/model/JPADataAccess.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/black/model/JPADataAccess.java
@@ -5,7 +5,6 @@
  *
  * Distributable under GPL license. See terms of license at gnu.org.
  */
-
 package ch.bfh.ti.soed.hs16.srs.black.model;
 
 import ch.bfh.ti.soed.hs16.srs.black.model.logic.Customer;
